2d09788d0df1b9296cee069963a6cf43a805a372
npoudel21 <npoudel21@my.whitworth.edu>
2020-01-07 19:14:34 -0800

    homework 1 complete

---
+++ EX1/Program.cs
@@ -0,0 +1,53 @@
+﻿//Novel Poudel
+//Homework 1
+//Exercise 1
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+//using this following namespace
+using System.Windows.Forms;
+
+namespace EX1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to this system.\nWhat do you want me to call you?");
+            string userName = Console.ReadLine();
+            Console.WriteLine("Hi, " + userName + ". How old are you?");
+            string ageString = Console.ReadLine();
+            int age = new int();
+            re:
+            //if ageString contains some non numerical characters, it goes to catch and asks the user to enter their age again
+            try
+            {
+                age = int.Parse(ageString);
+            }
+            catch
+            {
+                Console.WriteLine("Please enter your age.");
+                ageString = Console.ReadLine();
+                goto re;
+            }
+
+            if (age>=21)
+            {
+                Console.BackgroundColor = ConsoleColor.Yellow;
+                Console.Clear();
+                Console.ForegroundColor = ConsoleColor.Black;
+            }
+            else
+            {
+                Console.BackgroundColor = ConsoleColor.Red;
+                Console.Clear();
+                MessageBox.Show("Danger, " + userName + "Robinson! Danger!");
+                Console.Beep();
+            }
+            Console.WriteLine("Hello, " + userName + "!");
+        }
+    }
+}
---
+++ EX2/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace EX2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Calc cal = new Calc();
+            string option;
+            string num1, num2;
+            do
+            {
+            re:
+                Console.Clear();
+                Console.WriteLine("Enter two integers");
+                num1 = Console.ReadLine();
+                num2 = Console.ReadLine();
+                int n1 = 0;
+                int n2 = 0;
+                try
+                {
+                    n1 = int.Parse(num1);
+                    n2 = int.Parse(num2);
+                }
+                catch
+                {
+                    Console.WriteLine("Please enter integers");
+                    goto re;
+                }
+
+                Console.WriteLine("choose a number that represents the operation\n" +
+                    "\t1\tAdd\n" +
+                    "\t2\tMultiply\n" +
+                    "\t3\tSubtract\n" +
+                    "\t4\tDivide\n" +
+                    "\t0\tEXIT");
+                option = Console.ReadLine();
+                if (option == "1")
+                    MessageBox.Show(num1 + " + " + num2 + " = " + cal.add(n1, n2));
+                else if (option == "2")
+                    MessageBox.Show(num1 + " * " + num2 + " = " + cal.mul(n1, n2));
+                else if (option == "3")
+                    MessageBox.Show(num1 + " - " + num2 + " = " + cal.sub(n1, n2));
+                else if (option == "4")
+                    MessageBox.Show(num1 + " / " + num2 + " = " + cal.div(n1, n2));
+                else if (option == "0")
+                    break;
+                else
+                    Console.WriteLine("Please select an option");
+                Console.ReadLine();
+            } while (true);
+        }
+    }
+    class Calc
+    {
+        public int add(int a, int b) { return a + b; }
+        public int mul(int a, int b) { return a * b; }
+        public int sub(int a, int b) { return a - b; }
+        public double div(int a, int b) { return (double)a / b; }
+    }
+}
